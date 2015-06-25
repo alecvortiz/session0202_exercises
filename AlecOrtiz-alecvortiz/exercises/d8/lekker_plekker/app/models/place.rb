574cf101d4e7fb18d560022eea54f2cc07d1e673
Alexander Ortiz <alecvortiz@Alexanders-MacBook-Pro-7.local>
2015-06-26 01:19:37 +0200

    finished comments after a night of davaloning with derica

--- AlecOrtiz-alecvortiz/exercises/d8/lekker_plekker/app/models/place.rb
+++ AlecOrtiz-alecvortiz/exercises/d8/lekker_plekker/app/models/place.rb
@@ -1,2 +1,6 @@
 class Place < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
+	validates :name, :description, :rating, presence: true
+	validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 11 }
+	validates :name, uniqueness: { case_sensitive: false }
 end
